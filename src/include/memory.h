92abfd826ab27abace242aef66514dd0c5c59b7b
Cyrus-Gahatraj <cyrusgahatraj301@gmail.com>
2025-01-02 02:24:35 +0000

    Adding the strings datatype as object

--- src/include/memory.h
+++ src/include/memory.h
@@ -2,6 +2,12 @@
 #define MEMORY_H
 
 #include "main.h"
+#include "object.h"
+
+#define ALLOCATE(type, count) \
+    (type*)reallocate(NULL, 0, sizeof(type)*count)
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
 
 #define GROW_CAPACITY(capacity) \
     ((capacity) < 16 ? 16 : (capacity) * 1.5)
@@ -14,5 +20,6 @@
     reallocate(pointer, sizeof(type)*(oldCount), 0)
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void freeObjects();
 
 #endif
--- src/vm.c
+++ src/vm.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 
 #include "include/main.h"
 #include "include/compiler.h"
 #include "include/debug.h"
+#include "include/object.h"
+#include "include/memory.h"
 #include "include/vm.h"
 
 VM vm;
@@ -27,10 +30,11 @@ static void runtimeError(const char* format, ...){
 
 void initVM(){
     resetStack();
+    vm.objects = NULL;
 }
 
 void freeVM(){
-
+    freeObjects();
 }
 
 void push(Value value){
@@ -47,6 +51,19 @@ static Value peek(int distance){
 
 static bool isFalsey(Value value){
     return IS_NULL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static void concatenate(){
+    ObjString* b = AS_STRING(pop());
+    ObjString* a = AS_STRING(pop());
+
+    int length = a->length + b->length;
+    char* chars = ALLOCATE(char, length + 1);
+    memcpy(chars, a->chars, a->length+1);
+    memcpy(chars + a->length, b->chars, b->length);
+    chars[length] = '\0';
+    ObjString* result = takeString(chars, length);
+    push(OBJ_VAL(result));
 }
 
 static inline uint8_t READ_BYTE() {
@@ -110,8 +127,18 @@ static InterpretResult run() {
             case OP_LESS:{
                 BINARY_OP(BOOL_VAL, <); break;
             }
-            case OP_ADD:{
-                BINARY_OP(NUMBER_VAL, +);
+            case OP_ADD: {
+                if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+                    concatenate();
+                } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+                    double b = AS_NUMBER(pop());
+                    double a = AS_NUMBER(pop());
+                    push(NUMBER_VAL(a + b));
+                } else {
+                    runtimeError(
+                    "Operands must be two numbers or two strings.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
                 break;
             }
             case OP_SUBTRACT:{
